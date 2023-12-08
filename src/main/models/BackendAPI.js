a659da688c20e0f13c3b4a78a70859d7df00e7c9
eithan <eithaan13@gmail.com>
2023-12-08 16:58:33 +0100

    v0.5.1

--- src/main/models/BackendAPI.js
+++ src/main/models/BackendAPI.js
@@ -1,4 +1,5 @@
 import i18next from 'i18next';
+import log from 'electron-log';
 import LcuAPIinstance from './LcuApi';
 
 const { t } = i18next;
@@ -15,12 +16,14 @@ export const getSummonerData = async (summonerInternalName) => {
     const data = await resp.json();
 
     if (!data.success) {
+      log.error(`getSummonerData response error : ${data.error}`);
       return { success: false, error: data.error };
     }
 
     return { success: true, data: data.data };
   } catch (error) {
     // console.log('error', error);
+    log.error(`getSummonerData request error : ${error}`);
     return { success: false, error: t('backend.errors.internalError') };
   }
 };
@@ -29,6 +32,7 @@ export const getLobbyData = async (summoners) => {
   const { region } = await LcuAPIinstance.getRegion();
 
   const URL = `${process.env.REACT_APP_BACKEND_API_URL}/summoner/getLobby/${region}`;
+  console.log('URL', URL);
   try {
     const resp = await fetch(URL, {
       method: 'POST',
@@ -40,11 +44,13 @@ export const getLobbyData = async (summoners) => {
     const data = await resp.json();
 
     if (!data.success) {
+      log.error(`getLobbyData response error : ${data.error}`);
       return { success: false, error: data.error };
     }
 
     return { success: true, data: data.data };
   } catch (error) {
+    log.error(`getLobbyData request error : ${error}`);
     return { success: false, error: t('backend.errors.internalError') };
   }
 };
@@ -63,11 +69,13 @@ export const getLobbyRating = async (summoners, method) => {
     const data = await resp.json();
 
     if (!data.success) {
+      log.error(`getLobbyRating response error : ${data.error}`);
       return { success: false, error: data.error };
     }
 
     return { success: true, data: data.data.summoners };
   } catch (error) {
+    log.error(`getLobbyRating request error : ${error}`);
     return { success: false, error: t('backend.errors.internalError') };
   }
 };
